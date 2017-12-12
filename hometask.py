ec3524dc921ca99cba1f9fb830d66f64f6d75a1b
diplex2011 <33731052+diplex2011@users.noreply.github.com>
2017-12-12 16:45:26 +0200

    Create hometask.py

---
+++ hometask.py
@@ -0,0 +1,14 @@
+len(names)
+len(ages)
+len(text_a)
+sum(ages)
+max(ages)
+min(ages)
+print (names[0] +" is "+ str( ages[0] )+" years old ")
+print text_a.split(" ")[0]
+print text_b.split(" ")[5]
+len(text_b)
+new_text = text_a.split()
+min(new_text)
+new_text1 = text_b.split()
+max(new_text1)
